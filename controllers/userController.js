83c8811cc383d834ae053308a019e184bb53c461
Todd Lacher <toddl@pooleng.com>
2024-03-13 21:24:19 -0700

    fixed update user profile photo

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,18 @@
 import { StatusCodes } from "http-status-codes";
-import User from '../models/UserModel.js'
-import Job from '../models/JobModel.js'
-import cloudinary from 'cloudinary'
-import {promises as fs} from 'fs'
-
+import User from "../models/UserModel.js";
+import Job from "../models/JobModel.js";
+import cloudinary from "cloudinary";
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 export const getCurrentUser = async (req, res) => {
-  const user = await User.findOne({ _id: req.user.userId })
+  const user = await User.findOne({ _id: req.user.userId });
   //user.toJSON is method defined on UserModel to remove password
-  const userWithoutPassword = user.toJSON()
+  const userWithoutPassword = user.toJSON();
   res.status(StatusCodes.OK).json({ user: userWithoutPassword });
 };
 
 export const getApplicationStats = async (req, res) => {
-  const users = await User.countDocuments()
+  const users = await User.countDocuments();
   const jobs = await Job.countDocuments();
   res.status(StatusCodes.OK).json({ users, jobs });
 };
@@ -21,18 +20,20 @@ export const getApplicationStats = async (req, res) => {
 export const updateUser = async (req, res) => {
   //as an additional protection make sure password isn't in updateUser request by deleting it from the request
   const newUser = { ...req.body };
-  delete newUser.password
-  
+  delete newUser.password;
+
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path)
-    newUser.avatar = response.secure_url
-    newUser.avatarPublicId = response.public_id
+    const file = formatImage(req.file);
+
+    const response = await cloudinary.v2.uploader.upload(file);
+
+    newUser.avatar = response.secure_url;
+    newUser.avatarPublicId = response.public_id;
   }
-  const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser)
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser);
 
   if (req.file && updatedUser.avatarPublicId) {
-    await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId)
+    await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId);
   }
 
   res.status(StatusCodes.OK).json({ msg: "user update successful" });
